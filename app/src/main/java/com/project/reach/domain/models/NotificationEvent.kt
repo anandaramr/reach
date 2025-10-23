a52b21d30a202cf0ac8398cadc8460576f6ae08c
Anandaram R <anandaramr1915@gmail.com>
2025-10-24 00:49:44 +0530

    Update notification handling

--- app/src/main/java/com/project/reach/domain/models/NotificationEvent.kt
+++ app/src/main/java/com/project/reach/domain/models/NotificationEvent.kt
@@ -1,5 +1,10 @@
 package com.project.reach.domain.models
 
 sealed interface NotificationEvent {
-    data class Message(val username: String, val message: String, val timeStamp: Long): NotificationEvent
+    data class Message(
+        val userId: String,
+        val username: String,
+        val message: String,
+        val timeStamp: Long
+    ): NotificationEvent
 }
