dc927aa6c0f7ee8fd51c1d6b88d6377b7ac52acb
Anandaram R <anandaramr1915@gmail.com>
2025-10-25 20:34:53 +0530

    Add service validator to proto and add discovery packets to Packet.kt

--- app/src/main/java/com/project/reach/network/model/Packet.kt
+++ app/src/main/java/com/project/reach/network/model/Packet.kt
@@ -1,5 +1,6 @@
 package com.project.reach.network.model
 
+import com.project.reach.core.exceptions.UnknownSourceException
 import com.reach.project.core.serialization.ReachPacket
 
 sealed class Packet {
@@ -8,11 +9,25 @@ sealed class Packet {
         val username: String,
         val message: String,
         val timeStamp: Long = System.currentTimeMillis()
-    ) : Packet()
+    ): Packet()
 
     data class Typing(
         override val userId: String
-    ) : Packet()
+    ): Packet()
+
+    data class Hello(
+        override val userId: String,
+        val username: String
+    ): Packet()
+
+    data class Heartbeat(
+        override val userId: String,
+        val username: String
+    ): Packet()
+
+    data class GoodBye(
+        override val userId: String
+    ): Packet()
 
     abstract val userId: String
 
@@ -27,36 +42,56 @@ sealed class Packet {
          *
          * @throws IllegalArgumentException if the provided data cannot be deserialized
          * because it does not conform to the expected packet format.
+         *
+         * @throws UnknownSourceException if the received packet is from a different service
          */
         fun deserialize(bytes: ByteArray): Packet = PacketSerializer.deserialize(bytes)
     }
 }
 
 private object PacketSerializer {
-    private const val TYPE_MESSAGE = "message"
-    private const val TYPE_TYPING = "typing"
-
     fun serialize(packet: Packet): ByteArray {
-        val builder = ReachPacket.newBuilder().apply {
-            when (packet) {
-                is Packet.Message -> {
-                    type = TYPE_MESSAGE
-                    senderUuid = packet.userId
-                    senderUsername = packet.username
-                    payload = packet.message
-                    timestamp = packet.timeStamp
-                }
-                is Packet.Typing -> {
-                    type = TYPE_TYPING
-                    senderUuid = packet.userId
+        val builder = ReachPacket
+            .newBuilder()
+            .setServiceName(SERVICE_NAME)
+            .apply {
+                when (packet) {
+                    is Packet.Message -> {
+                        type = TYPE_MESSAGE
+                        senderUuid = packet.userId
+                        senderUsername = packet.username
+                        payload = packet.message
+                        timestamp = packet.timeStamp
+                    }
+                    is Packet.Typing -> {
+                        type = TYPE_TYPING
+                        senderUuid = packet.userId
+                    }
+                    is Packet.Heartbeat -> {
+                        type = TYPE_HEARTBEAT
+                        senderUuid = packet.userId
+                        senderUsername = packet.username
+                    }
+                    is Packet.Hello -> {
+                        type = TYPE_HELLO
+                        senderUuid = packet.userId
+                        senderUsername = packet.username
+                    }
+                    is Packet.GoodBye -> {
+                        type = TYPE_GOODBYE
+                        senderUuid = packet.userId
+                    }
                 }
             }
-        }
         return builder.build().toByteArray()
     }
 
     fun deserialize(bytes: ByteArray): Packet {
         val proto = ReachPacket.parseFrom(bytes)
+        if (proto.serviceName != SERVICE_NAME) {
+            throw UnknownSourceException()
+        }
+
         return when (proto.type) {
             TYPE_MESSAGE -> Packet.Message(
                 userId = proto.senderUuid,
@@ -64,10 +99,33 @@ private object PacketSerializer {
                 message = proto.payload,
                 timeStamp = proto.timestamp
             )
+
             TYPE_TYPING -> Packet.Typing(
                 userId = proto.senderUuid
             )
+
+            TYPE_HELLO -> Packet.Hello(
+                userId = proto.senderUuid,
+                username = proto.senderUsername
+            )
+
+            TYPE_HEARTBEAT -> Packet.Heartbeat(
+                userId = proto.senderUuid,
+                username = proto.senderUsername
+            )
+
+            TYPE_GOODBYE -> Packet.GoodBye(
+                userId = proto.senderUuid,
+            )
+
             else -> throw IllegalArgumentException("Unknown packet type: ${proto.type}")
         }
     }
+
+    private const val TYPE_MESSAGE = "message"
+    private const val TYPE_TYPING = "typing"
+    private const val TYPE_HELLO = "hello"
+    private const val TYPE_HEARTBEAT = "heartbeat"
+    private const val TYPE_GOODBYE = "goodbye"
+    private const val SERVICE_NAME = "REACH_SERVICE"
 }
