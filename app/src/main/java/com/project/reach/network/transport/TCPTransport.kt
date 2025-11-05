5c157b38a9e487650b2f474999a182314c39a7fe
Anandaram R <anandaramr1915@gmail.com>
2025-11-05 19:14:37 +0530

    Fix recursive update error and perform minor refactor

--- app/src/main/java/com/project/reach/network/transport/TCPTransport.kt
+++ app/src/main/java/com/project/reach/network/transport/TCPTransport.kt
@@ -2,7 +2,6 @@ package com.project.reach.network.transport
 
 import com.project.reach.network.model.NetworkPacket
 import com.project.reach.util.debug
-import com.project.reach.util.toByteArray
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -12,13 +11,14 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import java.io.InputStream
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.EOFException
 import java.net.InetAddress
 import java.net.ServerSocket
 import java.net.Socket
 import java.net.SocketException
 import java.net.SocketTimeoutException
-import java.nio.ByteBuffer
 import java.util.concurrent.ConcurrentHashMap
 
 class TCPTransport: NetworkTransport {
@@ -58,63 +58,54 @@ class TCPTransport: NetworkTransport {
         clientSocket: Socket
     ) = coroutineScope {
         val clientIp = clientSocket.inetAddress
-        connections.put(clientIp, clientSocket)
+        connections.putIfAbsent(clientIp, clientSocket)
 
-        var lastSeen = getCurrentTime()
-        clientSocket.soTimeout = SOCK_TIMEOUT
-        val input = clientSocket.inputStream
+        clientSocket.soTimeout = IDLE_TIMEOUT
+        val input = DataInputStream(clientSocket.inputStream)
 
         try {
             while (isActive && !clientSocket.isClosed) {
                 try {
-                    val isConnectionAlive = handleIncomingBytes(clientIp, input)
-                    if (!isConnectionAlive) break
-                    lastSeen = getCurrentTime()
+                    handleIncomingBytes(clientIp, input)
                 } catch (_: SocketTimeoutException) {
-                    if (getCurrentTime() - lastSeen > IDLE_TIMEOUT) {
-                        break
-                    }
+                    debug("Socket timeout: $clientIp")
+                    break
+                } catch (_: EOFException) {
+                    debug("Client disconnected: $clientIp")
+                    break
                 }
             }
         } catch (e: Exception) {
             debug("TCP Error handling client: $e")
         } finally {
-            connections.remove(clientIp)
+            debug("Disconnected from $clientIp")
+            connections.remove(clientIp, clientSocket)
             clientSocket.close()
         }
     }
 
-    private suspend fun handleIncomingBytes(clientIp: InetAddress, input: InputStream): Boolean {
-        val messageLength = ByteArray(4)
-        val bytes = input.read(messageLength)
-        if (bytes == -1) return false
-
-        val messageSize = ByteBuffer.wrap(messageLength).int
-        val messageBuffer = ByteArray(messageSize)
-        var bytesRead = 0
-        while (bytesRead < messageSize) {
-            val read = input.read(messageBuffer, bytesRead, messageSize - bytesRead)
-            if (read == -1) return false
-            bytesRead += read
-        }
+    private suspend fun handleIncomingBytes(
+        clientIp: InetAddress,
+        input: DataInputStream
+    ) {
+        val messageLength = input.readInt()
+        val messageBuffer = ByteArray(messageLength)
+        input.readFully(messageBuffer)
 
         _incomingPackets.emit(
             NetworkPacket(
                 address = clientIp,
-                payload = messageBuffer.copyOf(bytesRead)
+                payload = messageBuffer
             )
         )
-        return true
     }
 
     override suspend fun send(bytes: ByteArray, ip: InetAddress): Boolean {
-        try {
-            val socket = getOrCreateSocket(ip)
-            val output = socket.outputStream
+        val socket = getOrCreateSocket(ip)
+        val output = DataOutputStream(socket.outputStream)
 
-            // write output size first and then data
-            val size = bytes.size.toByteArray()
-            output.write(size)
+        try {
+            output.writeInt(bytes.size)
             output.write(bytes)
             output.flush()
             return true
@@ -132,7 +123,6 @@ class TCPTransport: NetworkTransport {
         return connections.computeIfAbsent(ip) { key ->
             Socket(key, NetworkTransport.PORT).also { socket ->
                 debug("connected to ${socket.inetAddress}")
-                connections.put(key, socket)
                 scope.launch {
                     handlePeerSocket(socket)
                 }
@@ -154,10 +144,7 @@ class TCPTransport: NetworkTransport {
         connections.clear()
     }
 
-    private fun getCurrentTime() = System.currentTimeMillis()
-
     companion object {
-        private const val SOCK_TIMEOUT = 10_000
         private const val IDLE_TIMEOUT = 60_000
     }
 }
